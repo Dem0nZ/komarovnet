77e7c32edc9ef305ea5fd6e2d64eb0ca48718746
Dem0nZ <cbpskov@gmail.com>
2021-03-19 20:31:10 +0300

    add second marker on map, fix width, computing center of map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,18 +4,30 @@ import { useEffect } from 'react';
 import { mainInfo } from '../models';
 
 
+const mapCenterLat = (mainInfo.mapOffice.lat + mainInfo.mapProm.lat) / 2;
+const mapCenterLon = (mainInfo.mapOffice.lon + mainInfo.mapProm.lon) / 2;
+
 const Map = () => {
     useEffect(()=>{
         const map = DG.map('map', {
-            'center': [ mainInfo.mapMarker.lat, mainInfo.mapMarker.lon ],
+            'center': [ mapCenterLat, mapCenterLon ],
             'zoom': 15,
             'zoomControl': false,
             'fullscreenControl': false,
         });
-        DG.marker([ mainInfo.mapMarker.lat, mainInfo.mapMarker.lon ]).addTo(map);
+        DG.marker([ mainInfo.mapOffice.lat, mainInfo.mapOffice.lon ])
+            .addTo(map)
+            .bindLabel('Офис', {
+                static: true
+            });
+        DG.marker([ mainInfo.mapProm.lat, mainInfo.mapProm.lon ])
+            .addTo(map)
+            .bindLabel('Производство', {
+                static: true
+            });
     })
     return (<div>
-        <div id='map' className='w-700 h-400 bg-red-400'>
+        <div id='map' className='w-850 h-400 bg-red-400'>
         </div>
     </div>
 
